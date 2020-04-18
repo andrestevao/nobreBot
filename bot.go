93a4159de219b32c648e71473669d05a1b57fce2
andrestevao <estevo.andre@gmail.com>
2020-04-18 02:34:22 -0300

    adding more spaghetti to spaghetti

--- bot.go
+++ bot.go
@@ -18,7 +18,8 @@ var (
 )
 
 var config = make(map[string]string)
-var commands = make(map[string]string)
+var simpleCommands = make(map[string]string)
+var users = make(map[string]int)
 
 // Find takes a slice and looks for an element in it. If found it will
 // return it's key, otherwise it will return -1 and a bool of false.
@@ -80,10 +81,82 @@ func basicDbConfig() {
 	}
 
 	if len(notFound) > 0 {
-		panic("Expected tables \""+strings.Join(notFound, ",")+"\" not found ")
+		var text string
+		for (text != "y" && text != "n") {
+			fmt.Println("Expected tables \""+strings.Join(notFound, ",")+"\" not found. Do you wish to create them? (y/n)")
+			reader := bufio.NewReader(os.Stdin)
+			text, _ = reader.ReadString('\n')
+			text = strings.TrimSpace(text)
+			text = strings.ToLower(text)
+		}
+
+		if strings.ToLower(text) == "n" {
+			panic("Cannot proceed without these tables created. Please check the HELPME file to create the tables manually or select Y next time to create the tables automatically.")
+		}
+		
+		queries := make(map[string]string)
+
+		queries["commands"] = `CREATE TABLE public.commands (
+			id serial NOT NULL,
+			privilege int NOT NULL,
+			command varchar NOT NULL,
+			type varchar NOT NULL,
+			response varchar NULL,
+			created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+			updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+			created_by text NOT NULL,
+			CONSTRAINT commands_pk PRIMARY KEY (id)
+		);`
+
+		queries["users"] = `CREATE TABLE public.users (
+			id serial NOT NULL,
+			privilege int NOT NULL,
+			name varchar NOT NULL,
+			created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+			updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+			created_by text NOT NULL,
+			CONSTRAINT users_pk PRIMARY KEY (id)
+		);`
+		
+		for _, missingTable := range expectedTables {
+			createQuery := queries[missingTable]
+			_, err := db.Query(createQuery)
+			if err != nil {
+				fmt.Println("Error while creating table \""+missingTable+"\":")
+				panic(err)
+			} else {
+				fmt.Println("Table \""+missingTable+"\" created sucessfully!")
+			}
+		}
+
 	}
 
 	
+}
+
+func loadCommands() {
+	//loading simple commands first
+	query := "SELECT command, response FROM commands WHERE type = 'simple';"
+	
+	rows, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var (
+			command string
+			response string
+		)	
+
+		if err := rows.Scan(&command, &response); err != nil {
+			panic(err)
+		}
+		
+		fmt.Println("command found: "+command)
+		simpleCommands[command] = response
+
+	}
 }
 
 func createConfig() {
@@ -130,10 +203,8 @@ func createConfig() {
 }
 
 func initIrc(nickname string, password string) {
-	// ircnick1 := "nobrebot"
 	ircnick1 := nickname
 	irccon := irc.IRC(ircnick1, "IRCTestSSL")
-	// irccon.Password = "oauth:m0lr6jx3vm8n6zaa358b8xyry37gpt"
 	irccon.Password = password
 
 	irccon.VerboseCallbackHandler = true
@@ -151,7 +222,7 @@ func initIrc(nickname string, password string) {
 		message := strings.TrimSpace(e.Message())
 		if strings.HasPrefix(message, "!"){
 			commandAndArguments := strings.Split(message, " ")
-			irccon.Privmsg(config["channel"], getCommand(commandAndArguments[0], commandAndArguments[1:]))			
+			irccon.Privmsg(config["channel"], getCommand(e.Nick, commandAndArguments[0], commandAndArguments[1:]))			
 		}
 	})
 	if err != nil {
@@ -162,12 +233,31 @@ func initIrc(nickname string, password string) {
 	irccon.Loop()
 }
 
-func getCommand(command string, arguments []string) string{
-	return "Command: "+command+"; Arguments:"+strings.Join(arguments, ",")
+func getCommand(user string, command string, arguments []string) string{
+	fmt.Println("Command: "+command+"; Arguments:"+strings.Join(arguments, ","))
+
+	//check if special command: add, remove, edit
+	specialCommands := []string{"!add", "!remove", "!edit"}
+	if _, found := Find(specialCommands, command); found{
+		specialCommand(user, command, arguments)
+	}
+
+	//else keep on it like it is a regular command
+	//to do
+	return "to-do"
+}
+
+func specialCommand(user string, command string, arguments []string) string {
+	if command == "!add" {
+		fmt.Println("todo")
+	}
+
+	return "to-do"
 }
 
 func main() {
 	createConfig()
 	initDb()
+	loadCommands()
 	initIrc(config["nickname"], config["password"])
 }
